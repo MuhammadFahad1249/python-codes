e12f75a6ae0196c51e51fa2b087d8550808c7fd5
Muhammad Fahad <92125039+MuhammadFahad1249@users.noreply.github.com>
2024-03-29 21:41:11 +0500

    Add files via upload

---
+++ array.py
@@ -0,0 +1,16 @@
+import array as arr
+
+# we can create array in two different ways one from using array keyword , another by using numpy 
+
+my_arr = arr.array('i',[1,2,3,4,5])
+print(my_arr)
+print(type(my_arr))
+
+
+
+# In numpy array it supports different data types 
+import numpy as nm
+my_arr1 = nm.array(["hello", 1, 2, 3])
+print(my_arr1)
+
+
